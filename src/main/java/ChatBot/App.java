d3c6358eec89a92bbee5a8aecf05adf57eb52a86
alexandre <bolotalex06@gmail.com>
2017-05-01 15:25:27 +0200

    Adding JavaFx View launching

--- src/main/java/ChatBot/App.java
+++ src/main/java/ChatBot/App.java
@@ -1,45 +1,42 @@
 package ChatBot;
 
-import ChatBot.model.Bot;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 /*................................................................................................................................
  . Copyright (c)
  .
  . The App	 Class was Coded by : Alexandre BOLOT
  .
- . Last Modified : 01/05/17 09:51
+ . Last Modified : 01/05/17 15:25
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
 
-public class App
+public class App extends Application
 {
     public static void main (String[] args)
-    {/*
-        DataBase db = Const.readDb();
+    {
+        launch(args);
+    }
     
-        Scanner scanner = new Scanner(System.in);
-    
-        String input = scanner.nextLine().trim();
-    
-        while (!input.equalsIgnoreCase("stop"))
-        {
-            Integer phraseKey = -1;
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public void start (Stage primaryStage) throws Exception
+    {
+        Rectangle2D screen = Screen.getPrimary().getVisualBounds();
         
-            for (Integer key : db.getPhrases().keySet())
-            {
-                if(db.getPhrases().get(key).equalsIgnoreCase(input)) phraseKey = key;
-            }
-        
-            if(db.getLinks().containsKey(phraseKey))
-            {
-                int returnPhrase = db.getLinks().get(phraseKey)[new Random().nextInt(db.getLinks().get(phraseKey).length)];
-                System.out.println(db.getPhrases().get(returnPhrase));
-            }
-        
-            input = scanner.nextLine().trim();
-        }*/
-    
-        new Bot();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ChatView.fxml"));
+        primaryStage.setTitle("ChatBot");
+        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.setResizable(false);
+        primaryStage.setX((screen.getWidth() - 400) / 2);
+        primaryStage.setY((screen.getHeight() - 400) / 2);
+        primaryStage.show();
     }
 }
