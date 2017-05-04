b1d5fc55c8f6c79c39dab6a10152932831681b0c
alexandre <bolotalex06@gmail.com>
2017-05-04 19:00:45 +0200

    Adding LearningController, used to teach new stuff to the ChatBot :)

---
+++ src/main/java/ChatBot/controller/LearningController.java
@@ -0,0 +1,19 @@
+package ChatBot.controller;
+
+import javafx.scene.control.TextField;
+
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The LearningController	 Class was Coded by : Alexandre BOLOT
+ .
+ . Last Modified : 04/05/17 18:59
+ .
+ . Contact : bolotalex06@gmail.com
+ ...............................................................................................................................*/
+
+public class LearningController
+{
+    public TextField txtTrigger;
+    public TextField txtAnswer;
+}
