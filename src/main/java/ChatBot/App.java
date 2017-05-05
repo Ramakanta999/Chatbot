9130ad7eeb260fa2bb931dfa794b068ddaa12c42
alexandre <bolotalex06@gmail.com>
2017-05-05 22:29:36 +0200

    Adding the LinksView.fxml launching + using Const attributes

--- src/main/java/ChatBot/App.java
+++ src/main/java/ChatBot/App.java
@@ -8,12 +8,14 @@ import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import static ChatBot.service.Const.*;
+
 /*................................................................................................................................
  . Copyright (c)
  .
  . The App	 Class was Coded by : Alexandre BOLOT
  .
- . Last Modified : 04/05/17 22:52
+ . Last Modified : 05/05/17 22:29
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
@@ -30,23 +32,32 @@ public class App extends Application
     public void start (Stage primaryStage) throws Exception
     {
         Rectangle2D screen = Screen.getPrimary().getVisualBounds();
-        
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ChatView.fxml"));
+    
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource(CHAT_VIEW_PATH));
         primaryStage.setTitle("ChatBot");
-        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.setScene(new Scene(root, CHAT_VIEW_WIDTH, CHAT_VIEW_HEIGHT));
         primaryStage.setResizable(false);
-        primaryStage.setX((screen.getWidth() - 400) / 2);
-        primaryStage.setY((screen.getHeight() - 400) / 2);
+        primaryStage.setX((screen.getWidth() - CHAT_VIEW_WIDTH) / 2);
+        primaryStage.setY((screen.getHeight() - CHAT_VIEW_HEIGHT) / 2);
     
         Stage stage2 = new Stage();
-        Parent root2 = FXMLLoader.load(getClass().getClassLoader().getResource("LearningView.fxml"));
+        Parent root2 = FXMLLoader.load(getClass().getClassLoader().getResource(LEARNING_VIEW_PATH));
         stage2.setTitle("Learning");
-        stage2.setScene(new Scene(root2, 300, 200));
+        stage2.setScene(new Scene(root2, LEARNING_VIEW_WIDTH, LEARNING_VIEW_HEIGHT));
         stage2.setResizable(false);
-        stage2.setX(primaryStage.getX() + 400);
+        stage2.setX(primaryStage.getX() + CHAT_VIEW_WIDTH);
         stage2.setY(primaryStage.getY());
     
+        Stage stage3 = new Stage();
+        Parent root3 = FXMLLoader.load(getClass().getClassLoader().getResource(LINKS_VIEW_PATH));
+        stage3.setTitle("Learning");
+        stage3.setScene(new Scene(root3, LINKS_VIEW_WIDTH, LINKS_VIEW_HEIGHT));
+        stage3.setResizable(false);
+        stage3.setX(primaryStage.getX() - LINKS_VIEW_WIDTH);
+        stage3.setY(primaryStage.getY());
+    
         stage2.show();
+        stage3.show();
         primaryStage.show();
     }
 }
