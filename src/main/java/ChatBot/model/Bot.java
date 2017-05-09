9129e3679bdf33bc040f04e661c87d5640b0b2a9
alexandre <bolotalex06@gmail.com>
2017-05-09 08:36:43 +0200

    Adding notion of Command Line and now using updateDb() to sync with file

--- src/main/java/ChatBot/model/Bot.java
+++ src/main/java/ChatBot/model/Bot.java
@@ -1,47 +1,57 @@
 package ChatBot.model;
 
 import ChatBot.service.Const;
+import ChatBot.service.Const.CommandLine;
 import ChatBot.service.Const.ReplacementCode;
 
 import java.util.Random;
 
+import static ChatBot.service.Const.CommandLine.valueOf;
 import static ChatBot.service.Const.EXIT_INDEX;
+import static ChatBot.service.Const.ReplacementCode.CHATBOT_NAME;
+import static ChatBot.service.Const.ReplacementCode.USER_NAME;
+import static ChatBot.service.Const.readDb;
 
 /*................................................................................................................................
  . Copyright (c)
  .
  . The Bot	 Class was Coded by : Alexandre BOLOT
  .
- . Last Modified : 08/05/17 11:59
+ . Last Modified : 09/05/17 08:36
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
 
 public class Bot
 {
-    private DataBase db;
+    private DataBase db = readDb();
     
     public Bot ()
     {
-        db = Const.readDb();
+        db = readDb();
     }
     
     public String getBotAnswer (String input)
     {
-        db = Const.readDb();
-        
-        //Removing punctuation (for now)
+        db = readDb();
+    
+        //region --> Removing punctuation
         while (input.matches("^.*[.,;! ]$"))
         {
             input = input.substring(0, input.length() - 1);
         }
+        //endregion
     
-        //Needed to get in the While at first try
-        String outputPhrase = input;
-    
+        //region --> Detecting if command line
+        if(input.matches("^/.*")) return applyCommand(input.substring(1));
+        //endregion
+        
         //region --> Case order to stop
         if(db.findUserPhrase(input) == EXIT_INDEX) System.exit(0);
         //endregion
+    
+        //Needed to get in the While at first try
+        String outputPhrase = input;
         
         //We do not want to repeat the input phrase
         while (outputPhrase.equalsIgnoreCase(input))
@@ -62,8 +72,8 @@ public class Bot
     
     public String getBotPhrase (int poolIndex)
     {
-        db = Const.readDb();
-    
+        db = readDb();
+        
         String botPhrase = db.getBotPhrase(poolIndex);
     
         return replaceCode(botPhrase);
@@ -77,5 +87,37 @@ public class Bot
         }
         
         return string;
+    }
+    
+    private String applyCommand (String command)
+    {
+        try
+        {
+            String[] split = command.split(":", 2);
+            
+            CommandLine commandLine = valueOf(split[0].trim());
+            
+            switch (commandLine)
+            {
+                case ChangeBotName:
+                    CHATBOT_NAME.setValue(split[1].trim());
+                    break;
+                case ChangeUserName:
+                    USER_NAME.setValue(split[1].trim());
+                    break;
+                case Exit:
+                    System.exit(0);
+                    break;
+                case Help:
+                    return Const.getHelpMessage();
+            }
+            
+            return "Command executed";
+        }
+        catch (IllegalArgumentException iae)
+        {
+            iae.printStackTrace();
+            return "Command not found";
+        }
     }
 }
