3d9b9c486cf54e9ba65dd8230ca9c6e264ee1514
alexandre <bolotalex06@gmail.com>
2017-05-12 13:58:22 +0200

    Adding JavaDoc

--- src/main/java/ChatBot/service/App.java
+++ src/main/java/ChatBot/service/App.java
@@ -13,11 +13,15 @@ import static ChatBot.service.Const.*;
  .
  . The App	 Class was Coded by : Alexandre BOLOT
  .
- . Last Modified : 09/05/17 22:52
+ . Last Modified : 12/05/17 13:58
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
 
+/**
+ This class is the entry point of the applications.<br>
+ It starts the JavaFX Application and displays the first panel views.
+ **/
 public class App extends Application
 {
     public static void main (String[] args)
@@ -33,7 +37,7 @@ public class App extends Application
         primaryStage.setTitle("ChatBot");
         primaryStage.setScene(new Scene(root, CHAT_VIEW_WIDTH, CHAT_VIEW_HEIGHT));
         primaryStage.setResizable(false);
-    
+        
         primaryStage.show();
     }
 }
