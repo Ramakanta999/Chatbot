559d6bcb283b95869d164990d1aa9253f4879d79
alexandre <bolotalex06@gmail.com>
2017-09-20 11:00:02 +0200

    Update JavaDoc

--- src/main/java/ChatBot/controller/LinksController.java
+++ src/main/java/ChatBot/controller/LinksController.java
@@ -12,11 +12,19 @@ import javafx.scene.layout.AnchorPane;
  .
  . The LinksController	 Class was Coded by : Alexandre BOLOT
  .
- . Last Modified : 05/05/17 22:26
+ . Last Modified : 20/09/17 10:59
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
 
+/**
+ This Class the controller of LinksView.fxml.<br>
+ It allows the user to look for the possible answers linked to an input.<br>
+ <br>
+ It handles the events triggered by the GUI.<br>
+ <br>
+ __ Dependency : model.DataBase, service.Const __
+ */
 public class LinksController
 {
     public AnchorPane       currentPane;
@@ -25,6 +33,14 @@ public class LinksController
     public TextField        txtInput;
     public Label            lblError;
     
+    /**
+     Event Handler of search button : Triggered on a click of that button.<br>
+     Shows in GUI bot User's and Bot's known linked entries.<br>
+     <br>
+     __ Warning : Reads db __
+     <br>
+     __ Dependency : model.Bot __
+     */
     public void search_onAction ()
     {
         lblError.setVisible(false);
